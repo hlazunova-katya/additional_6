549117b7297acce8c05842059c18f11b8599e4e7
hlazunova-katya <katrin_glazunova@mail.ru>
2017-09-22 14:45:04 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 module.exports = function zeros(expression) {
   // your solution
+    expression=expression.split('*');
+
+    const doubleFactNums=[];
+    const factNums=[];
+    const buffer=[];
+    const result=[];
+    let checkEven = false;
+
+    for (let i=0; i<expression.length; i++){
+        if (expression[i].includes('!!')){
+            doubleFactNums.push(parseInt(expression[i]));
+        }
+        else factNums.push(parseInt(expression[i]));
+    }
+    for (let i=0; i<doubleFactNums.length; i++){
+        if (doubleFactNums[i]%2===0){
+            checkEven=true;
+        }
+    }
+    if (!checkEven&&!factNums.length) return 0;
+
+    for (let i=0; i<factNums.length; i++){
+        for (let j=factNums[i]; j>0; j--){
+            buffer.push(j);
+        }
+    }
+    for (let i=0; i<doubleFactNums.length; i++){
+        for (let j=doubleFactNums[i]; j>=1;){
+            buffer.push(j);
+            j-=2;
+        }
+    }
+
+    for (let i=0; i<buffer.length; i++){
+        if (buffer[i]%25===0){
+            buffer.push(5);
+            buffer.push(5);
+        }
+        else if (buffer[i]%5===0){
+            result.push(5);
+        }
+    }
+    return result.length;
 }
