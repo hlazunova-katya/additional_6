824741342593f5a53418b522deb81a64c97f9a9e
hlazunova-katya <katrin_glazunova@mail.ru>
2017-09-22 15:03:19 +0300

    comments added and small refactor

--- src/index.js
+++ src/index.js
@@ -1,26 +1,32 @@
 module.exports = function zeros(expression) {
   // your solution
     expression=expression.split('*');
-
+    //if there are double factorials to find
     const doubleFactNums=[];
+    //if there are simple factorials to find
     const factNums=[];
+    //a buffer for factors
     const buffer=[];
     const result=[];
-    let checkEven = false;
-
+    //sorting numbers, depending on what king of factorial we need to find
     for (let i=0; i<expression.length; i++){
         if (expression[i].includes('!!')){
             doubleFactNums.push(parseInt(expression[i]));
         }
         else factNums.push(parseInt(expression[i]));
     }
-    for (let i=0; i<doubleFactNums.length; i++){
-        if (doubleFactNums[i]%2===0){
-            checkEven=true;
+    //if we have only uneven double factorials to find return 0
+    //cause they have no zeroes at all
+    if (!factNums.length){
+        let checkEven = false;
+        for (let i=0; i<doubleFactNums.length; i++){
+            if (doubleFactNums[i]%2===0){
+               checkEven = true;
+            }
         }
+        if (!checkEven) return 0;
     }
-    if (!checkEven&&!factNums.length) return 0;
-
+    //getting factors
     for (let i=0; i<factNums.length; i++){
         for (let j=factNums[i]; j>0; j--){
             buffer.push(j);
@@ -32,15 +38,16 @@ module.exports = function zeros(expression) {
             j-=2;
         }
     }
-
+    //getting factors with 5 on the end
     for (let i=0; i<buffer.length; i++){
         if (buffer[i]%25===0){
-            buffer.push(5);
-            buffer.push(5);
+            result.push(5);
+            result.push(5);
         }
         else if (buffer[i]%5===0){
             result.push(5);
         }
     }
+    //return the number of 5s
     return result.length;
 }
